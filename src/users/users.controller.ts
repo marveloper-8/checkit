92a20eaffa4316c3e6f47e0ca4a5c6f530d90b13
marveloper-8 <marveloper.8@gmail.com>
2024-12-28 06:45:55 -0800

    users module and controllers

---
+++ src/users/users.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
+import { ApiBearerAuth, ApiOperation, ApiTags } from "@nestjs/swagger";
+import { UsersService } from "./users.service";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { AuthGuard } from "@nestjs/passport";
+import { RolesGuard } from "src/common/guards/roles.guard";
+import { Roles } from "src/common/decorators/roles.decorators";
+import { UserRole } from "@prisma/client";
+
+@ApiTags('users')
+@Controller('users')
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Post()
+    @ApiOperation({summary: 'Create new user'})
+    create(@Body() createUserDto: CreateUserDto) {
+        return this.usersService.create(createUserDto)
+    }
+
+    @Get()
+    @ApiBearerAuth()
+    @UseGuards(AuthGuard('jwt'), RolesGuard)
+    @Roles(UserRole.ADMIN)
+    @ApiOperation({summary: 'Get all users (Admin only)'})
+    findAll() {
+        return this.usersService.findAll()
+    }
+}
