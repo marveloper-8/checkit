0a75765c073cdd78e9fbdcc1e1576262438d2c2b
marveloper-8 <marveloper.8@gmail.com>
2024-12-28 16:00:26 -0800

    app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { OrdersModule } from './orders/orders.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    OrdersModule,
+    ChatModule
+  ],
 })
 export class AppModule {}
---
+++ src/orders/orders.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { ChatModule } from "src/chat/chat.module";
+import { OrdersService } from "./orders.service";
+import { OrdersController } from "./orders.controller";
+
+@Module({
+    imports: [ChatModule],
+    providers: [OrdersService],
+    controllers: [OrdersController],
+    exports: [OrdersService]
+})
+export class OrdersModule {}
