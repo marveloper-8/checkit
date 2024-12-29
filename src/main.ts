7dd426d18f4695e6a323e06872d6bc9c67c89a33
marveloper-8 <marveloper.8@gmail.com>
2024-12-28 23:33:32 -0800

    main file setup

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api')
-  await app.listen(process.env.PORT ?? 3000);
-  console.log(`Application is running on: http://localhost:3000/api`)
+  
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+    forbidNonWhitelisted: true
+  }))
+
+  app.useGlobalFilters(new HttpExceptionFilter())
+
+  app.useGlobalInterceptors(new LoggingInterceptor())
+
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN || '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  })
+
+  const config = new DocumentBuilder()
+    .setTitle('Order Management API')
+    .setDescription('API documentation for Order Management System with Chat Feature')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
+  await app.listen(process.env.PORT || 3000)
 }
 bootstrap();
